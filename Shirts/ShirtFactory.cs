c33a2c272d7cac78797d7fcbb675233e64803eef
cstern96 <chayala.ys@gmail.com>
2024-05-23 20:45:48 -0400

    Homework 51

---
+++ Shirts/ShirtFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace Shirts
+{
+    internal class ShirtFactory
+    {
+        // I used more like 45 minutes just because I haven't used c# in a while and I had to familiarize myself with it again.
+       // Also, I know that the best form is that nothing is supposed to be in main I just couldn't figure out how to do it in another class in the time given.
+        static void Main(string[] args)
+        {
+            string[] color = { "red", "green", "blue" };
+            string[] pattern = { "striped", "checked", "plain" };
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    new Shirt(color[i], pattern[j]);
+                    Console.WriteLine($" {color[i]} : {pattern[j]}");
+                }
+            }
+        }
+    }
+}
---
+++ Shirts/shirt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shirts
+{
+    public class Shirt
+    {
+        public string ShirtColor { get; set; }
+        public string ShirtPattern { get; set; }
+
+        public Shirt(string shirtColor, string shirtPattern)
+        {
+            this.ShirtColor = shirtColor;
+            this.ShirtPattern = shirtPattern;
+        }
+    }
+}
