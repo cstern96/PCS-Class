7744508c05d746d94ae4032d797f6c0ba5888b1a
cstern96 <chayala.ys@gmail.com>
2024-07-28 12:23:00 -0400

    64

---
+++ Javascript/64/info.js
@@ -0,0 +1,5 @@
+'use strict';
+const myName = 'Chayala'
+const email = 'chayala.ys@gmail.com'
+let food = prompt("Whats your favorite food")
+console.log(food)
---
+++ Javascript/64/read.js
@@ -0,0 +1,3 @@
+'use strict';
+console.log(myName)
+console.log(email)
