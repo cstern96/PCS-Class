138f7f281299ae37e5def328bd790e5d52497334
cstern96 <chayala.ys@gmail.com>
2024-08-01 17:20:21 -0400

    66

---
+++ Javascript/66/Hw.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const someLetters = ['A', 'B', 'C', 'D'];
+
+/*function checkLetters(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i].toUpperCase()) {
+            return false
+        }
+    }
+    return true;
+}
+console.log(checkLetters(someLetters));*/
+
+function myEvery(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!callback(arr[i])) {
+            return false
+        }
+    }
+    return true;
+}
+const uppercase = letter => letter === letter.toUpperCase();
+const lowercase = letter => letter === letter.toLowerCase();
+console.log(myEvery(someLetters, uppercase));
+console.log(someLetters.every(uppercase));
+console.log(myEvery(someLetters, lowercase));
+console.log(someLetters.every(lowercase));
+
+
+
+function mySome(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(mySome(someLetters, uppercase));
+console.log(someLetters.some(uppercase));
+console.log(mySome(someLetters, lowercase));
+console.log(someLetters.some(lowercase));
+
+function myOnlyIf(arr, myFilter, action){
+    for(let i = 0; i < arr.length; i++) {
+        if (myFilter(arr[i])){
+            action(arr[i])
+        }
+    }
+}
+
+
+
+
