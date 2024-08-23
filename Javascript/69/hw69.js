bcef8c9d1622195c9aae5c507883c05008a04499
cstern96 <chayala.ys@gmail.com>
2024-08-23 13:24:06 -0400

    69

---
+++ Javascript/69/hw69.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+    const button1 = document.querySelector('#button1');
+    let number = 2;
+    button1.addEventListener('click', function () {
+        const myNewButton = document.createElement('button');
+        myNewButton.className = 'inline-button';
+        document.body.appendChild(myNewButton);
+        myNewButton.innerText = number;
+        number++;
+    })
+
+}());
