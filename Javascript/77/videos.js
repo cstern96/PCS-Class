cdfa057073d153632bd15d058ad0222511e4d826
cstern96 <chayala.ys@gmail.com>
2024-09-26 17:25:19 -0400

    77

---
+++ Javascript/77/videos.js
@@ -0,0 +1,49 @@
+/* global $*/
+// I couldn't get it to work but I'm goint to watch the video and see how you got it to work. I will also try to go to lab tonight. 
+(async function () {
+    'use strict';
+    try {
+        const r = await fetch('videos.json');
+        if (!r.ok) {
+            throw new Error(`${r.status} - ${r.statusText}`);
+        }
+        const loadedVideos = await r.json();
+        const videoDisplay = document.querySelector('#videos');
+        loadedVideos.forEach(video => {
+            videoDisplay.innerHTML += `<div class="video-item" data-id="${video.id}">
+                    <h2>${video.name}</h2>
+                    <img src="${video.image}" alt="${video.name}" style="max-width: 300px; height: auto;">
+                </div>`;
+        });
+    } catch (e) {
+        console.error(e);
+    }
+    $(document).on('click', '.video-item', async function () {
+        try {
+            const clickedVideoId = $(this).data('id');
+            const r = await fetch(`${clickedVideoId}.json`);
+            if (!r.ok) {
+                throw new Error(`${r.status} - ${r.statusText}`);
+            }
+            const videoDisplay = await r.json();
+            displayVideo(videoDisplay);
+        }
+        catch (e) {
+            console.error(e);
+        }
+
+
+    });
+    function displayVideo(video) {
+        const videoDisplay = document.querySelector('#videoDisplay');
+
+        videoDisplay.innerHTML = `<h2>${video.name}</h2>
+                            <video controls autoplay width="500">
+                            <source src="${video.url}" type="video/mp4">
+                            </video>`;
+    }
+
+
+
+
+}());
