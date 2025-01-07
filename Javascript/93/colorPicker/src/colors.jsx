24506e09490491451409ff567641402a2d5a7f4f
cstern96 <chayala.ys@gmail.com>
2025-01-06 20:34:13 -0500

    93

--- Javascript/93/colorPicker/src/colors.jsx
+++ Javascript/93/colorPicker/src/colors.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 
 export default function Colors() {
     const [state, setState] = useState({
-        background: 'white',
-        font: 'black'
+        background: '',
+        fontColor: '',
+        font: ''
     });
 
     const colorChanged = c => {
@@ -12,21 +13,31 @@ export default function Colors() {
             [c.target.name]: c.target.value
         })
     }
+    
 
     return (
         <>
             <form>
                 <label htmlFor='background'>Background Color  </label>
                 <input type="color" name="background" value={state.background} onChange={colorChanged} />
-                <label htmlFor="font">Font Color</label>
-                <input type="color" name="font" value={state.font} onChange={colorChanged} />
+                <label htmlFor="fontColor"> Font Color </label>
+                <input type="color" name="fontColor" value={state.fontColor} onChange={colorChanged} />
+                <label htmlFor="font"> Font </label>
+                <select name="font" value={state.font} onChange={colorChanged}>
+                    <option value="Arial">Arial</option>
+                    <option value="Times New Roman">Times New Roman</option>
+                    <option value="Calibri">Calibri</option>
+                    <option value=" Lucida Handwriting ">Lucida Handwriting</option>
+                    <option value="Brush Script MT">Brush Script MT</option>
+                </select>
             </form>
 
             <div
                 style={{
                     backgroundColor: state.background,
-                    color: state.font,                
-                    minHeight: '100vh',         
+                    color: state.fontColor,                
+                    minHeight: '100vh',    
+                    fontFamily: state.font     
                 }}
             >
                 This is a preview text!
